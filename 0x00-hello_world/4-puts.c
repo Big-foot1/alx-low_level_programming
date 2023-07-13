710b59b812282dca7511946430a0d7d76d027ac2
Big-foot1 <imutyaba11@gmail.com>
2023-07-13 14:36:19 -0700

    display to output using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 
+/*this is where the program starts*/
 int main(void)
 {
 	/*Display to standard output using put*/
 	char my_strg[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(my_strg);
 	return (0);
 }
