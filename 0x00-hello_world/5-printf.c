1206303e0669d0f68bc710a8691137f2c1b81977
Big-foot1 <imutyaba11@gmail.com>
2023-07-13 14:53:18 -0700

    display with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - printing to standard output using printf function
+ * not allowed to us puts function
+ * return: zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
