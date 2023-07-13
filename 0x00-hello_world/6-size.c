602535e087c976279f546ba1d9913b29579d579a
Big-foot1 <imutyaba11@gmail.com>
2023-07-13 16:32:38 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(q));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(s));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(t));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(p));
-return (0);
+return (0);
 }
