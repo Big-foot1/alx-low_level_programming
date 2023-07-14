181aecd4b5b1fb369c4d91952e7bb97ea467b4e7
Big-foot1 <imutyaba11@gmail.com>
2023-07-14 13:49:01 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - program prints alphabet in lowercase
+ * in reverse order
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'z'
+	
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
