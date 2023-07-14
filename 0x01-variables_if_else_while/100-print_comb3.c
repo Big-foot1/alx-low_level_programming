ad6a332e5479a2c07bfdf6f6b0b38a765f6ba668
Big-foot1 <imutyaba11@gmail.com>
2023-07-14 15:34:01 -0700

    combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,10 @@ int main(void)
 
 	for (i = 0, j = 0; i < 10 && j < 10; i++, j++)
 	{
-		if (i != j)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
