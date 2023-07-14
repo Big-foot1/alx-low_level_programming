22c90f2f5aa9625304b980fef90ac49ec2e4bf35
Big-foot1 <imutyaba11@gmail.com>
2023-07-14 09:27:49 -0700

    if statement with double condition

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,13 +20,13 @@ int main(void)
 	/*check if last digit of n is less or greater than 5*/
 	if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n,l);
-	else if (n < 6 && n != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,l);
-	}
 	else if (n == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n,l);
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,l);
 	}
 	return (0);
 }
