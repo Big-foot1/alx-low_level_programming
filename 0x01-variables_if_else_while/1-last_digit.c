3827bfa7811a5e80e2272796d2d4c1c852e9a449
Big-foot1 <imutyaba11@gmail.com>
2023-07-14 09:13:33 -0700

    if statement with double condition

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,19 +5,21 @@
 /**
  * main - program will assign random number to
  * variable n each time it is executed
- * test whether last digit of n 
+ * test whether last digit of n
  * is greater or less than 5
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
+	int l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	l = n % 10
 	/*check if last digit of n is less or greater than 5*/
 	if (n > 5)
-		printf("Last digit of %d and is greater than 5\n", n);
+		printf("Last digit of %d is %c and is greater than 5\n", n,l);
 	else if (n < 6 && n != 0)
 	{
 		printf("Last digit of %d and is less than 6 and not 0\n", n);
