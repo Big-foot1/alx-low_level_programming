659d7bd3e5dc67f343fd7fcff4d68ffd27f56fe4
Big-foot1 <imutyaba11@gmail.com>
2023-07-13 17:03:27 -0700

    display without puts or printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char stng[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, stng, sizeof(my_strng) - 1);
+	write(1, stng, sizeof(stng) - 1);
 	return (1);
 }
