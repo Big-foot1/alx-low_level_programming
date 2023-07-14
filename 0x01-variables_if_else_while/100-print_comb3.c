b5e6c35693fd2645454e4cd66caced27e2b05556
Big-foot1 <imutyaba11@gmail.com>
2023-07-14 15:13:43 -0700

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - program prints a combination of 2 digits
+ * leaving out similar combitions
+ * assuming 01 is simialar to 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0, j = 0; i < 100 && j < 100; i++, j++)
+	{
+		if (i != j && ij != ji)
+		{
+			putchar(ij + '0');
+			while (ij < 90)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
