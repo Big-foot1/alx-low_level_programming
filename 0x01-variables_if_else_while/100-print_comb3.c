e911e97a3db7c859d92bf923724e370148f9c6dd
Big-foot1 <imutyaba11@gmail.com>
2023-07-15 11:42:13 -0700

    printing double digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,10 +16,10 @@ int main(void)
 		{
 			if (!((i == j) || (j > i)))
 			{
-				putchar(j + '0');
-				putchar(i + '0');
-				if ((i <= '9' && j <= '8'))
+				if (i <= '9' && j <= '8')
 				{
+					putchar(j + '0');
+					putchar(i + '0');
 					putchar(',');
 					putchar(' ');
 				}
