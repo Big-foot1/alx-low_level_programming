96070f95a80d96f67b2312550cef9df5a4327d51
Big-foot1 <imutyaba11@gmail.com>
2023-07-17 04:06:32 -0700

    combination of 2 2 digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - program all posible combinations of two two digits
+ * two numbers are seperated by a space
+ * after every combination must be a seperating comma
+ * combinations must be in ascending order
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int m, n;
+	int x = 0;
+
+	for (m = 0; m < 99; m++)
+	{
+		for (n = 0; n < 100; n++)
+		{
+			if (m < 10 && n < 10)
+			{
+				putchar(x);
+				putchar(m + '0');
+				putchar(' ');
+				putchar(x);
+				putchar(n + '0');
+			}
+			else
+			{
+				putchar(m + '0');
+				putchar(' ');
+				putchar(n + '0');
+			}
+		}
+	}
+	return (0);
+}
+				
