2310531adcae7c1ce6e37fc497882a50d5763785
Big-foot1 <imutyaba11@gmail.com>
2023-07-17 05:07:28 -0700

    combination of 2 2 digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (m = 0; m < 99; m++)
 	{
-		for (n = 1; n < 100; n++)
+		for (n = m + 1; n < 100; n++)
 		{
 			putchar('0' + (m / 10));
 			putchar('0' + (m % 10));
 			putchar(' ');
 			putchar('0' + (n / 10));
 			putchar('0' + (n % 10));
-			if (m < 98)
+			if (m < 98 && n < 99)
 			{
 				putchar(',');
 				putchar(' ');
