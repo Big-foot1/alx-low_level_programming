a081e5dca8e74ccabd65a7959bb4af304c12469f
Big-foot1 <imutyaba11@gmail.com>
2023-07-18 20:02:28 -0700

    printing natural numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	int i, j;
 
-	if (n <  98)
+	if (n <=  98)
 	{
 		for (i = 0; i <= 98; i++)
 		{
@@ -27,6 +27,5 @@ void print_to_98(int n)
 			_putchar(',');
 			_putchar(' ');
 		}
-	}
-	
+	}	
 }
