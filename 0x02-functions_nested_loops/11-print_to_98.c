31535ebdd470c7265826cc44a01e3526d158ea7b
Big-foot1 <imutyaba11@gmail.com>
2023-07-19 10:48:46 -0700

    printing natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 	if (n <=  98)
 	{
-		for (i = 0; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (j = 0; j > 98; j--)
+		for (j = n; j > 98; j--)
 		{
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
