a3b92c066761e84d23ccafaa597a0d42a2b4949b
Big-foot1 <imutyaba11@gmail.com>
2023-07-19 11:31:16 -0700

    printing natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,32 +10,43 @@ void print_to_98(int n)
 {
 	int i, j;
 
-	if (n <=  98)
+	for (i = n; i < 98; i++)
 	{
-		for (i = n; i < 98; i++)
+		if (i <= 98)
 		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			if (i < 98)
 			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
-			else if (i == n)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
+		}
+		else if (i == 98)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 		}
 	}
-	else if (n >= 98)
+	for (j = n; j > 98; j--)
 	{
-		for (j = n; j > 98; j--)
+		if (j >= 100)
 		{
-			_putchar((j / 10) + '0');
+			_putchar((j / 100) + '0');
+			_putchar(((j / 10) % 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
-	}	
+		else if (j >= 98 && j < 100)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			if (j > 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
 }
