434f8181a6225c0a396004935ce421f280b6a8d5
Big-foot1 <imutyaba11@gmail.com>
2023-07-24 11:57:50 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "stdio.h"
+#include "string.h"
+
+/**
+ * _strlen - funvtion checks length of a string
+ * @s: string passed to the function
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	char end_char = '\0';
+	int i = 0;
+
+	while (*(s + i) != end_char)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_rev - printint string in reverse
+ * @s: character passed to function
+ * Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	for ((i = _strlen(s) - 1); i >= 0; i--)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
