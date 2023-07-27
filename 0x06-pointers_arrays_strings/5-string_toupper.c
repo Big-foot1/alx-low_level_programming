4c58189109bddeafb36adeaac8088648090caf96
Big-foot1 <imutyaba11@gmail.com>
2023-07-27 12:21:19 -0700

    convert lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -19,9 +19,12 @@ char *string_toupper(char *str)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] = str[i] - 32;
-			i++;
+			return (str[i] - ('a' - 'A'));
+		}
+		else
+		{
+			return (str[i]);
 		}
 	}
 	return (str);
-}
+}[
