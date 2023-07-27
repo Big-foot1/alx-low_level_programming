458554b7dd9e3871f2e9de6e93196f1196c1d978
Big-foot1 <imutyaba11@gmail.com>
2023-07-27 05:55:42 -0700

    printing reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+#include "string.h"
+
+/**
+ * reverse_array - function prints array in reverse order
+ * @a: array passed to the function
+ * @n: size of array passed to the function
+ * Return: reverse array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int aray;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		aray = *(a + i);
+		*(a + i) = a[n];
+		a[n] = aray;
+	}
+}
