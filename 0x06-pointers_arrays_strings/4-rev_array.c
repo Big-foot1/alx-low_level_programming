dd53b1c91d1b240dcace58cd07a6f1e078806ee8
Big-foot1 <imutyaba11@gmail.com>
2023-07-27 06:03:26 -0700

    printing reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -17,7 +17,7 @@ void reverse_array(int *a, int n)
 	for (i = 0; i < n - 1; i++)
 	{
 		aray = *(a + i);
-		*(a + i) = *a[n];
-		*a[n] = aray;
+		*(a + i) = *(a + (n - 1));
+		*(a + (n - 1)) = aray;
 	}
 }
