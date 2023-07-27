579e05443c7355ab906f9ddc044095cdf1a3736c
Big-foot1 <imutyaba11@gmail.com>
2023-07-27 09:49:03 -0700

    convert lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -15,13 +15,13 @@ char *string_toupper(char *str)
 
 	i = 0;
 
-	while (*(str + i) != end_char)
+	while (str[i] != end_char)
 	{
-		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			*(str + i) = *(str + i) - 32;
+			str[i] = str[i] - 32;
 			i++;
 		}
 	}
-	return (*(str + i);
+	return (str);
 }
