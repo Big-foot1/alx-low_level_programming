5601ba46858eb60a931b514a2209ecbf74173c7f
Big-foot1 <imutyaba11@gmail.com>
2023-07-27 00:09:53 -0700

    copy characters of string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,24 +3,6 @@
 #include "string.h"
 
 /**
- * _strlen - function outs length of a string
- * @s: number passed to the function
- * Return: length of the string
- */
-
-int _strlen(char *s)
-{
-	char end_char = '\0';
-	int i = 0;
-
-	while (*(s + i) != end_char)
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
  * _strncpy - function copies n characters from source string
  * @dest: String passed to the function
  * @src: string passed to the function
@@ -33,12 +15,12 @@ char *_strncpy(char *dest, char *src, int n)
 	char end_char = '\0';
 	int i = 0;
 
-	while (_strlen(src[i]) < n)
+	while (i < n)
 	{
 		dest[i] = end_char;
 		i++;
 	}
-	while ((_strlen(src[i]) < n) && src[i] != end_char)
+	while (i > n && src[i] != end_char)
 	{
 		dest[i] = src[i];
 		i++;
