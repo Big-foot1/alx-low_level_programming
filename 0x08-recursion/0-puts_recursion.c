354c32bc9f3bc47db09f957683c4e8196eba6dfa
Big-foot1 <imutyaba11@gmail.com>
2023-08-02 13:48:17 -0700

    puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 
 /**
  * _puts_recursion - function prints strings and terminates with new line
- * @s - string character passed to the function
+ * @s:  string character passed to the function
  * Return: 0
  */
 
@@ -11,9 +11,9 @@ void _puts_recursion(char *s)
 {
 	if (*s <= '\0')
 	{
+		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-	_putchar('\n');
 }
