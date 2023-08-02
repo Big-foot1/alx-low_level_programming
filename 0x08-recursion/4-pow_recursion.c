7c0a01213884116f1227892085fb617c963530d9
Big-foot1 <imutyaba11@gmail.com>
2023-08-02 15:05:09 -0700

    exponential or powers with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * _pow_recursion - function prints the exponential of given integer
+ * @x: base integer passed to the function
+ * @y: exponential integer passed to the function
+ * Return: -1 if n is less than 0 otherwise result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x,(y - 1)));
+}
