3083956904c824cdadcf86a542a8d256355fec9c
Big-foot1 <imutyaba11@gmail.com>
2023-08-16 14:06:58 -0700

    printing name withfunction pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
 #ifndef POINTER_FUNC_H
 #define POINTER_FUNC_H
-#include "stddef.h"
+#include <stddef.h>
 
 /**
- * this is our main.h header file
+ * this is a header file
  * it contain all our prototype files 
  * to be used in this project
  */
@@ -12,11 +12,6 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 int _putchar(char c);
 
 #endif
